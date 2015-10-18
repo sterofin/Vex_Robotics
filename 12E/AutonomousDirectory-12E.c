c0ad13d6465e31b80ce2752e16e657b34835dea1
sterofin <sterofinstophat@gmail.com>
2015-10-18 13:51:44 -0400

    Scoring Autonomae

--- 12E/AutonomousDirectory-12E.c
+++ 12E/AutonomousDirectory-12E.c
@@ -14,18 +14,22 @@ void blueRight(int delayer)
 void redLeft(int delayer)
 {
 	wait1Msec(delayer*1000);
-	move(100); // move forward for 3.5 secs
-	wait1Msec(3500);
-	move(0); // Stop moving
+	move(127); // move forward for 3 secs
+	wait1Msec(3000);
+	move(40); // move slower for 2 secs
 	Outspeed(65); // start the outtake
+	wait1Msec(2000);
+	move(0); // stop
 	wait1Msec(500); // wait half a second
-	move(-100); // move backwards for .15 seconds
-	wait1Msec(150);
+	/*
+	move(-100); // move backwards for .5 seconds
+	wait1Msec(500);
 	move(0); // stop moving
 	wait1Msec(500); // wait  half a second
-	strafeRight(100); // strafe right for .8 seconds
-	wait1Msec(800);
+	strafeRight(100); // strafe right for .75 seconds
+	wait1Msec(750);
 	strafeRight(0); // stop strafing
+	*/
 	wait1Msec(1000); // wait 1 second
 	Inspeed(127); // start the intake
 	wait1Msec(500); // begin shhoting cycle
@@ -43,10 +47,6 @@ void redLeft(int delayer)
 	wait1Msec(500);
 	Inspeed(0);
 	Outspeed(0);
-
-	/*
-	rotateRight(100); // rotate to the right for half a second
-	*/
 }
 
 void redRight(int delayer)
