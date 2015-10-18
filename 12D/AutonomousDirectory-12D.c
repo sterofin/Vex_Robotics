a2e54ed2b63a2be042506f638649106504a98d02
sterofin <sterofinstophat@gmail.com>
2015-10-18 09:53:37 -0400

    Combined 12E and 12D

---
+++ 12D/AutonomousDirectory-12D.c
@@ -0,0 +1,55 @@
+#include "MovementDirectory-12D.c";
+
+
+void blueLeft(int delayer)
+{
+	wait1Msec(delayer*1000);
+}
+
+void blueRight(int delayer)
+{
+	wait1Msec(delayer*1000);
+}
+
+void redLeft(int delayer)
+{
+	wait1Msec(delayer*1000);
+	move(100); // move forward for 3.5 secs
+	wait1Msec(3500);
+	move(0); // Stop moving
+	Outspeed(65); // start the outtake
+	wait1Msec(500); // wait half a second
+	move(-100); // move backwards for .15 seconds
+	wait1Msec(150);
+	move(0); // stop moving
+	wait1Msec(500); // wait  half a second
+	strafeRight(100); // strafe right for .8 seconds
+	wait1Msec(800);
+	strafeRight(0); // stop strafing
+	wait1Msec(1000); // wait 1 second
+	Inspeed(127); // start the intake
+	wait1Msec(500); // begin shhoting cycle
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	Outspeed(0);
+
+	/*
+	rotateRight(100); // rotate to the right for half a second
+	*/
+}
+
+void redRight(int delayer)
+{
+	wait1Msec(delayer*1000);
+}
---
+++ 12D/LcdCode-12D.c
@@ -0,0 +1,297 @@
+#include "AutonomousDirectory-12D.c";
+const short leftButton = 1;
+const short centerButton = 2;
+const short rightButton = 4;
+
+int colorCount = 0;
+int sideCount = 0;
+int delayCount = 0;
+int count = 0;
+int delayer1 = 0;
+int autonCount = 0;
+bool lcdTerminate = false;
+bool ifRed = false;
+bool ifBlue = false;
+bool ifLeft = false;
+bool ifRight = false;
+
+//Wait for Press--------------------------------------------------
+void waitForPress()
+{
+	while(nLCDButtons == 0){}
+	wait1Msec(5);
+}
+//----------------------------------------------------------------
+
+//Wait for Release------------------------------------------------
+void waitForRelease()
+{
+	while(nLCDButtons != 0){}
+	wait1Msec(5);
+}
+
+void lcdPreAuton()
+{
+	//Clear LCD
+	clearLCDLine(0);
+	clearLCDLine(1);
+	//Loop while center button is not pressed
+	while(lcdTerminate == false)
+	{
+		while(nLCDButtons != centerButton)
+		{
+			switch(colorCount)
+			{
+				case 0:
+					//Display first choice
+					displayLCDCenteredString(0, "Red");
+					displayLCDCenteredString(1, "<		 Enter		>");
+					waitForPress();
+					//Increment or decrement "count" based on button press
+					if(nLCDButtons == leftButton)
+					{
+						waitForRelease();
+						colorCount = 1;
+					}
+					else if(nLCDButtons == rightButton)
+					{
+						waitForRelease();
+						colorCount = 1;
+					}
+					ifRed = true;
+					ifBlue = false;
+					break;
+				case 1:
+					//Display first choice
+					displayLCDCenteredString(0, "Blue");
+					displayLCDCenteredString(1, "<		 Enter		>");
+					waitForPress();
+					//Increment or decrement "count" based on button press
+					if(nLCDButtons == leftButton)
+					{
+						waitForRelease();
+						colorCount = 0;
+					}
+					else if(nLCDButtons == rightButton)
+					{
+						waitForRelease();
+						colorCount = 0;
+					}
+					ifBlue = true;
+					ifRed = false;
+					break;
+			}
+		}
+		wait10Msec(10);
+		//next go to side selection
+		waitForRelease();
+		if(ifBlue == true && ifRed == false)
+		{
+			while(nLCDButtons != centerButton)
+			{
+				switch(sideCount)
+				{
+					case 0:
+						displayLCDCenteredString(0, "Left");
+						displayLCDCenteredString(1, "<		 Enter		>");
+						waitForPress();
+						if(nLCDButtons == leftButton)
+						{
+							waitForRelease();
+							sideCount = 1;
+						}
+						else if(nLCDButtons == rightButton)
+						{
+							waitForRelease();
+							sideCount = 1;
+						}
+						autonCount = 0;
+						break;
+					case 1:
+						displayLCDCenteredString(0, "Right");
+						displayLCDCenteredString(1, "<		 Enter		>");
+						waitForPress();
+						if(nLCDButtons == leftButton)
+						{
+							waitForRelease();
+							sideCount = 0;
+						}
+						else if(nLCDButtons == rightButton)
+						{
+							waitForRelease();
+							sideCount = 0;
+						}
+						autonCount = 1;
+						break;
+				}
+			}
+		}
+		else if(ifBlue == false && ifRed == true)
+		{
+			while(nLCDButtons != centerButton)
+			{
+				switch(sideCount)
+				{
+					case 0:
+						displayLCDCenteredString(0, "Left");
+						displayLCDCenteredString(1, "<		 Enter		>");
+						waitForPress();
+						if(nLCDButtons == leftButton)
+						{
+							waitForRelease();
+							sideCount = 1;
+						}
+						else if(nLCDButtons == rightButton)
+						{
+							waitForRelease();
+							sideCount = 1;
+						}
+						autonCount = 2;
+						break;
+					case 1:
+						displayLCDCenteredString(0, "Right");
+						displayLCDCenteredString(1, "<		 Enter		>");
+						waitForPress();
+						if(nLCDButtons == leftButton)
+						{
+							waitForRelease();
+							sideCount = 0;
+						}
+						else if(nLCDButtons == rightButton)
+						{
+							waitForRelease();
+							sideCount = 0;
+						}
+						autonCount = 3;
+						break;
+				}
+			}
+		}
+		wait10Msec(10);
+		waitForRelease();
+		while(nLCDButtons !=centerButton)
+		{
+		switch(delayCount){
+		case 0:
+			//Display first choice
+			displayLCDCenteredString(0, "Delay");
+			displayLCDCenteredString(1, "< 0 seconds >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				delayCount = 3;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				delayCount++;
+			}
+			delayer1 = 0;
+			break;
+		case 1:
+			//Display second choice
+			displayLCDCenteredString(0, "Delay");
+			displayLCDCenteredString(1, "< 3 seconds >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				delayCount--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				delayCount++;
+			}
+			delayer1 = 3;
+			break;
+		case 2:
+			//Display third choice
+			displayLCDCenteredString(0, "Delay");
+			displayLCDCenteredString(1, "< 5 seconds >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				delayCount--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				delayCount++;
+			}
+			delayer1 = 5;
+			break;
+		case 3:
+			//Display fourth choice
+			displayLCDCenteredString(0, "Delay");
+			displayLCDCenteredString(1, "< 7 seconds >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				delayCount--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				delayCount = 0;
+			}
+			delayer1 = 7;
+			break;
+		default:
+			delayCount = 0;
+			break;
+		}
+		lcdTerminate = true;
+	}
+}
+}
+//------------- End of User Interface Code ---------------------/
+
+void lcdAuton()
+{
+	//Declare count variable to keep track of our choice
+	int count = 0;
+  //------------- Beginning of Robot Movement Code ---------------
+	//Clear LCD
+	clearLCDLine(0);
+	clearLCDLine(1);
+	//Switch Case that actually runs the user choice
+	switch(autonCount)
+	{
+	case 0:
+		//If count = 0, run the code correspoinding with choice 1
+		displayLCDCenteredString(0, "Blue Left");
+		displayLCDCenteredString(1, "is running!");
+		blueLeft(delayer1);
+		break;
+	case 1:
+		//If count = 1, run the code correspoinding with choice 2
+		displayLCDCenteredString(0, "Blue Right");
+		displayLCDCenteredString(1, "is running!");
+		blueRight(delayer1);
+		break;
+	case 2:
+		//If count = 2, run the code correspoinding with choice 3
+		displayLCDCenteredString(0, "Red Left");
+		displayLCDCenteredString(1, "is running!");
+		redLeft(delayer1);
+		break;
+	case 3:
+		//If count = 3, run the code correspoinding with choice 4
+		displayLCDCenteredString(0, "Red Right");
+		displayLCDCenteredString(1, "is running!");
+		redRight(delayer1);
+		break;
+	default:
+		displayLCDCenteredString(0, "No valid choice");
+		displayLCDCenteredString(1, "was made!");
+		break;
+	}
+}
