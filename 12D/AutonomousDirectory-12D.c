96cd0a1010190ff433999bf7f0ebe715918e107a
sterofin <sterofinstophat@gmail.com>
2015-10-18 12:10:58 -0400

    Program fixes

--- 12D/AutonomousDirectory-12D.c
+++ 12D/AutonomousDirectory-12D.c
@@ -4,27 +4,19 @@
 void blueLeft(int delayer)
 {
 	wait1Msec(delayer*1000);
-}
-
-void blueRight(int delayer)
-{
-	wait1Msec(delayer*1000);
-}
-
-void redLeft(int delayer)
-{
-	wait1Msec(delayer*1000);
-	move(100); // move forward for 3.5 secs
-	wait1Msec(3500);
-	move(0); // Stop moving
+	move(100); // move forward for 2.5 secs
+	wait1Msec(2500);
+	move(20); // move slower for 1.5 secs
 	Outspeed(65); // start the outtake
+	wait1Msec(1500);
+	move(0); // stop
 	wait1Msec(500); // wait half a second
-	move(-100); // move backwards for .15 seconds
-	wait1Msec(150);
+	move(-100); // move backwards for .4 seconds
+	wait1Msec(400);
 	move(0); // stop moving
 	wait1Msec(500); // wait  half a second
-	strafeRight(100); // strafe right for .8 seconds
-	wait1Msec(800);
+	strafeRight(100); // strafe right for .75 seconds
+	wait1Msec(750);
 	strafeRight(0); // stop strafing
 	wait1Msec(1000); // wait 1 second
 	Inspeed(127); // start the intake
@@ -43,13 +35,112 @@ void redLeft(int delayer)
 	wait1Msec(500);
 	Inspeed(0);
 	Outspeed(0);
+}
 
-	/*
-	rotateRight(100); // rotate to the right for half a second
-	*/
+void blueRight(int delayer)
+{
+	wait1Msec(delayer*1000);
+	move(100); // move forward for 2.5 secs
+	wait1Msec(2500);
+	move(20); // move slower for 1.5 secs
+	Outspeed(65); // start the outtake
+	wait1Msec(1500);
+	move(0); // stop
+	wait1Msec(500); // wait half a second
+	move(-100); // move backwards for .4 seconds
+	wait1Msec(400);
+	move(0); // stop moving
+	wait1Msec(500); // wait  half a second
+	strafeLeft(100); // strafe left for .75 seconds
+	wait1Msec(750);
+	strafeLeft(0); // stop strafing
+	wait1Msec(1000); // wait 1 second
+	Inspeed(127); // start the intake
+	wait1Msec(500); // begin shhoting cycle
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	Outspeed(0);
+}
+
+void redLeft(int delayer)
+{
+	wait1Msec(delayer*1000);
+	move(100); // move forward for 2.5 secs
+	wait1Msec(2500);
+	move(20); // move slower for 1.5 secs
+	Outspeed(65); // start the outtake
+	wait1Msec(1500);
+	move(0); // stop
+	wait1Msec(500); // wait half a second
+	move(-100); // move backwards for .4 seconds
+	wait1Msec(400);
+	move(0); // stop moving
+	wait1Msec(500); // wait  half a second
+	strafeRight(100); // strafe right for .75 seconds
+	wait1Msec(750);
+	strafeRight(0); // stop strafing
+	wait1Msec(1000); // wait 1 second
+	Inspeed(127); // start the intake
+	wait1Msec(500); // begin shhoting cycle
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	Outspeed(0);
 }
 
 void redRight(int delayer)
 {
 	wait1Msec(delayer*1000);
+	move(100); // move forward for 2.5 secs
+	wait1Msec(2500);
+	move(20); // move slower for 1.5 secs
+	Outspeed(65); // start the outtake
+	wait1Msec(1500);
+	move(0); // stop
+	wait1Msec(500); // wait half a second
+	move(-100); // move backwards for .4 seconds
+	wait1Msec(400);
+	move(0); // stop moving
+	wait1Msec(500); // wait  half a second
+	strafeLeft(100); // strafe left for .75 seconds
+	wait1Msec(750);
+	strafeLeft(0); // stop strafing
+	wait1Msec(1000); // wait 1 second
+	Inspeed(127); // start the intake
+	wait1Msec(500); // begin shhoting cycle
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	wait1Msec(1000);
+	Inspeed(127);
+	wait1Msec(500);
+	Inspeed(0);
+	Outspeed(0);
 }
